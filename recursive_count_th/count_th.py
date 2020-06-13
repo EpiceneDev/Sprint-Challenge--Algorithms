46d1c78c1dcf66e1940d2fae5f33fde144d8c376
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-13 10:28:40 -0700

    count_th done

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -10,4 +10,23 @@ def count_th(word):
     if len(word) < 2:
         return 0
 
-    
+    # use find() method. https://www.geeksforgeeks.org/python-string-find/
+    #   "returns the lowest index of the substring if it is found in given string. 
+    #       If it’s not found then it returns -1."
+    elif word.find("th") >= 0:
+        # store the index where the "th" is found
+        found = word.find("th") 
+
+        # since find returns the lowest index of the substring,
+        # add 2 to go to the start of the next index to check
+        new_count = found +2
+
+        # identify where to start the next search for recursion
+        new_word = word[new_count: len(word)]
+
+        # when "th" is found, go to the next set to check.
+        return 1 + count_th(new_word)
+
+    # if it is not found (word.find() returns -1)
+    else:
+        return 0
